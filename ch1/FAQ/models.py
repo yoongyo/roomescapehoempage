70abcb25015da5958902913a7e0ae05a11596545
sun0511 <sun0511777@gmail.com>
2019-03-18 23:20:11 +0900

    second

---
+++ ch1/FAQ/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from froala_editor.fields import FroalaField
+
+class Qna(models.Model):
+        title = models.CharField(max_length=50)
+        text = FroalaField(theme='dark')
